3c0854815830e9801c7d65392896a7fb727da157
MaxBrombach <48799491+MaxBrombach@users.noreply.github.com>
2024-01-16 15:29:04 +0100

    doing some stuff

--- UIstuff.py
+++ UIstuff.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 
 
 class UIInsertionsort:
@@ -7,10 +8,15 @@ class UIInsertionsort:
         pass
 
     def createButtonArray(self):
-        col1, col2, col3, col4, col5 = st.columns((1,1,1,1,1))
+        col1, col2, col3, col4, col5 = st.columns((1, 1, 1, 1, 1))
         columnlist = [col1, col2, col3, col4, col5]
-        buttonvalues = list()
-        for i in range(5):
-            buttonvalues.append(columnlist[i].button("⠀"+str(i)+"⠀"))
+        if 'buttonarray' not in st.session_state:
+            st.session_state['buttonarray'] = np.zeros(6)
+        sesbuttonvalues = st.session_state['buttonarray']
+        print("jooo",sesbuttonvalues)
 
-        st.write(buttonvalues)
+        for i in range(1, 6):
+            sesbuttonvalues[i-1] = (columnlist[i-1].button("⠀" + str(i) + "⠀"))
+
+        st.write(sesbuttonvalues)
+
