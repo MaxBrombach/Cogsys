e64368db109d8d3ded4d65ee9130090115205e59
MaxBrombach <48799491+MaxBrombach@users.noreply.github.com>
2024-01-16 18:31:04 +0100

    use hardcoded array

--- UIstuff.py
+++ UIstuff.py
@@ -6,7 +6,7 @@ from numpy.core._multiarray_umath import dtype
 class UIInsertionsort:
 
     def __init__(self):
-        pass
+        self.startarray = [2, 4, 3, 8, 7]
 
     def createButtonArray(self):
         col1, col2, col3, col4, col5 = st.columns((1, 1, 1, 1, 1))
@@ -18,8 +18,8 @@ class UIInsertionsort:
         buttonvalues = np.zeros(5, dtype=bool)
         st.write("Sessions", st.session_state['buttonarray'])
 
-        for i in range(1, 6):
-            buttonvalues[i-1] = (columnlist[i-1].button("⠀" + str(i) + "⠀"))
+        for i in range(5):
+            buttonvalues[i] = (columnlist[i].button("⠀" + str(self.startarray[i]) + "⠀"))
 
         st.session_state['buttonarray'] = np.logical_or(st.session_state['buttonarray'], buttonvalues)
         st.write(st.session_state['buttonarray'])
