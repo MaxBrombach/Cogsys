e7d5dfa26bdfb96a0eb44f6d8ab0c1bb99ee05b0
MaxBrombach <48799491+MaxBrombach@users.noreply.github.com>
2024-01-30 16:54:45 +0100

    Tutorlogic, change main

--- Usermodel.py
+++ Usermodel.py
@@ -5,14 +5,19 @@ class Usermodel:
 
     def __init__(self):
         self.fehler = 0
-        self.setMaxtries = 3
+        self.maxtries = 3
         self.tries = 0
 
 
     def resetMaxtries(self):
-        self.setMaxtries = 0
+        self.maxtries = 0
     def increaseAttempt(self):
         self.tries += 1
+
+    def maxTriesachieved(self):
+        if self.maxtries == self.tries:
+            return True
+        return False
 
     def handlestuff(self):
         pass
